1463d7246e146c27e2f22f7002a21973c983b48a
novzhho <zh15738796281@outlook.com>
2023-07-31 10:52:52 +0800

    new file:   Java/test2_12.java

---
+++ Java/test2_12.java
@@ -0,0 +1,10 @@
+
+
+public class test2_12 {
+    public static void main(String[] args){
+        System.out.println("today is Tue,so 100 days is");
+        int days = 100%7+2;
+        System.out.println("周" + days);
+
+    }
+}

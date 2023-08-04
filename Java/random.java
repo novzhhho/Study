67e562b66b202ea1f52682d54cf97d7cf01b3b4c
novzhho <zh15738796281@outlook.com>
2023-08-04 14:50:43 +0800

    modified:   Java/random.java

--- Java/random.java
+++ Java/random.java
@@ -1,6 +1,6 @@
 public class random {
     public static void main(String[] args) {
-        int num = (int)(Math.random() * 12 + 1 );
+        int num = (int)(Math.random() * 12 + 1 );           //random生成的是大于0小于1的无限位数字，这很重要
         System.out.println("生成的随机数的大小为：" + num);
     }
 }

6877103c4481a10d8231e7da39128e886cfb6ee5
novzhho <zh15738796281@outlook.com>
2023-08-11 21:13:22 +0800

    modified:   qq.html

---
+++ Vue/vue-project/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3>第一次写Vue</h3>
+  <p> {{mag}} </p>
+  <p>{{csnnhtml}}</p>
+  <p v-html="csnnhtml"></p>
+</template>
+
+
+<script >
+export default{
+  data(){
+    return{
+      mag:"测试一下，神奇的语法",
+      csnnhtml:"<a href= 'https://baidu.com'>百度</a>"
+
+    }
+  }
+}
+
+</script>
+
+
+
+
